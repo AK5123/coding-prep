23c164d0a995aa9b403aa3bc69950f06aa887225
ak5123 <kanduarul@gmail.com>
2021-07-23 23:52:11 +0530

    update bst

--- data_strucures/bst.py
+++ data_strucures/bst.py
@@ -39,9 +39,12 @@ class Node:
       print( self.data),
       if self.right:
          self.right.PrintTree()
-root = Node(12)
-root.insert(6)
-root.insert(14)
-root.insert(3)
-print(root.findval(7))
-print(root.findval(14))
+         
+# Driver Code
+if __name__ == "__main__":
+    root = Node(12)
+    root.insert(6)
+    root.insert(14)
+    root.insert(3)
+    print(root.findval(7))
+    print(root.findval(14))
