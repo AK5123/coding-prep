49a4313ae974a1b55fc15360ef64d701eba660d7
ak5123 <kanduarul@gmail.com>
2021-07-24 23:58:57 +0530

    bst fix

--- data_strucures/bst.py
+++ data_strucures/bst.py
@@ -39,12 +39,11 @@ class Node:
       print( self.data),
       if self.right:
          self.right.PrintTree()
-         
+
 # Driver Code
 if __name__ == "__main__":
     root = Node(12)
     root.insert(6)
     root.insert(14)
-    root.insert(3)
     print(root.findval(7))
     print(root.findval(14))
