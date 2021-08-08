cd5a4ff5ec4a8516895217fb9f9cb32770584f74
ak5123 <kanduarul@gmail.com>
2021-08-08 23:56:16 +0530

    np

---
+++ solved_problems/nextPermutation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def nextPermutation(self, n):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(n)-1,0,-1):
+            if n[i-1] < n[i]:
+                sm = i
+                for j in range(i+1,len(n)):
+                    if n[j] > n[i-1] and n[j] < n[sm]:
+                        sm = j
+                n[i-1],n[sm] = n[sm],n[i-1]
+                temp = [k for k in n[i:]]
+                temp.sort()
+                for k in range(len(temp)):
+                    n[i+k] = temp[k]
+                return    
+        n.reverse()       
+        return        
