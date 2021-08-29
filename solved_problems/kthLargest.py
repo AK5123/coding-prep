37182a8c96c5e2f4cb499a97da59b391cdb195a0
ak5123 <kanduarul@gmail.com>
2021-08-29 23:08:01 +0530

    k-lar

---
+++ solved_problems/kthLargest.py
@@ -0,0 +1,21 @@
+import random
+class Solution:
+    def findKthLargest(self, n, k):
+        size = len(n)
+        st = 0
+        lt = size -1
+        while st <= lt:
+            j = st
+            ran = random.randint(st,lt)
+            n[ran],n[lt] = n[lt],n[ran]
+            for i in range(st,lt):
+                if n[i] <= n[lt]:
+                    n[j],n[i] = n[i],n[j]
+                    j += 1
+            n[j],n[lt] = n[lt],n[j]
+            if j == (size - k):
+                return n[j]
+            elif j > (size - k):
+                lt = j - 1
+            else:
+                st = j + 1
