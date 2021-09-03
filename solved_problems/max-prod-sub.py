6ba98ef3958fd018c7f1d57403e85ddf78a6a846
ak5123 <kanduarul@gmail.com>
2021-09-03 23:13:34 +0530

    mas-prod

--- solved_problems/max-prod-sub.py
+++ solved_problems/max-prod-sub.py
@@ -1,1 +1,14 @@
 # https://leetcode.com/problems/maximum-product-subarray/
+class Solution:
+    def maxProduct(self, a):
+        res = a[0]
+        cmin = a[0]
+        cmax = a[0]
+        for i in a[1:]:
+            temp = cmax
+            cmax = max(max(cmax*i,cmin*i),i)
+            cmin = min(min(temp*i,cmin*i),i)
+            if res < cmax:
+                res = cmax
+        return res        
+       
