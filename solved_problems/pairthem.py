b3238f2ea55eae66b17cb86c0806e440f7fcbd23
ak5123 <kanduarul@gmail.com>
2021-09-18 23:09:50 +0530

    pm-fix

--- solved_problems/pairthem.py
+++ solved_problems/pairthem.py
@@ -1,1 +1,19 @@
 # https://www.hackerearth.com/challenges/competitive/hackerearth-test-draft-3-115/problems/f788eeedf61444858d1607d83961988e/
+def good_pairs (n, arr):
+    # Write your code here
+    d={}
+    for c in arr:
+        d.setdefault(c,0)
+        d[c] += 1
+    res = 0
+    for i in d.values():
+        if i > 1:
+            res += (i*(i-1)/2)
+    return int(res)
+    pass
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+out_ = good_pairs(n, arr)
+print (out_)
