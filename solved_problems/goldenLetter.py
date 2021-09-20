58eceb5ec5fd4e8c557192830f1cd0cd9d556615
ak5123 <kanduarul@gmail.com>
2021-09-21 01:00:58 +0530

    gl-fix

--- solved_problems/goldenLetter.py
+++ solved_problems/goldenLetter.py
@@ -1,3 +1,4 @@
+# https://www.hackerearth.com/challenges/competitive/hackerearth-test-draft-3-115/problems/7a32605de0d14c96b296bc6d2aacc9f4/
 def golden_char (key, st):
     # Write your code here
     d = set()
